e04d03c8317cacbe33d4bfac7736e719e76e9fee
Alvaro De La Torre <alvarodltp@gmail.com>
2018-06-21 17:23:28 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,6 +18,7 @@ def first_challenge
       value.each do |flavor|
         delete_if flavor == "strawberry"
       end
+    end
 
   #remember to return your newly altered contacts hash!
   contacts
